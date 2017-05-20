a13b439653e91866a0ea700614fdd2715dc4316c
duivvv <geoffrey.dhuyvetters@gmail.com>
2017-05-20 12:24:35 +0200

    :bookmark: v2.0.0

--- index.js
+++ index.js
@@ -1,78 +1,23 @@
-const fs = require(`fs`);
-const path = require(`path`);
-const chalk = require(`chalk`);
-
-const isValidName = require(`./lib/isValidName`);
+const buildPlugins = require(`./lib/buildPlugins`);
+const registerPlugins = require(`./lib/registerPlugins`);
 
 module.exports.register = (server, options, next) => {
 
   const {
     path: p,
     plugins = [],
-    log = true,
-    pluginOptions = {}
+    pluginOptions = {},
+    log = true
   } = options;
 
-  const register = (f, isModule = true, isDir = false) => {
-
-    const plugin = require(f);
-
-    const name = isModule ? f : path.basename(f, `.js`);
-
-    server.register({
-
-      register: plugin,
-      options: pluginOptions[name]
-
-    }, err => {
-
-      if (err) console.error(err);
-
-      else {
-
-        if (log) {
-
-          const base = isModule ? `` : `${path.basename(p)}/`;
-          const ext = isModule ? `` : isDir ? `/index.js` : `.js`;
-          const n = `${base}${name}${ext}`;
-
-          console.log(
-            `${chalk.yellow(`hapi-devine-autoload`)}: registered ${chalk.cyan(`'${n}'`)}`
-          );
-
-        }
-
-      }
-
-    });
-
-
-  };
-
   if (log) console.log(``);
 
-  plugins.forEach(p => {
-    register(p.register.attributes.pkg.name, true);
-  });
-
-  if (p && fs.existsSync(p)) {
-    fs.readdirSync(p).forEach(f => {
-
-      if (f.startsWith(`_`)) return;
-
-      const ff = path.join(p, f);
-      const isDir = fs.lstatSync(ff).isDirectory();
-
-      if (!isDir && !isValidName(f)) return;
-
-      register(ff, false, isDir);
-
+  buildPlugins({arr: plugins, folder: p, options: pluginOptions})
+    .then(plugins => registerPlugins(plugins, server, log))
+    .then(() => {
+      if (log) console.log(``);
+      return next();
     });
-  }
-
-  if (log) console.log(``);
-
-  next();
 
 };
 
